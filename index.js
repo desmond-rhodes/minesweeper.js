ce9a8611b80ff9c473e65508bf7e379c0ddbb0b7
Desmond Rhodes <desmond.rhodes@outlook.com>
2022-06-01 09:05:29 +0700

    Mines

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 const d_game_width = document.getElementById('game-width')
 const d_game_height = document.getElementById('game-height')
-const d_mine = document.getElementById('mine')
+const d_mine_amount = document.getElementById('mine-amount')
 const d_cell_width = document.getElementById('cell-width')
 const d_cell_height = document.getElementById('cell-height')
 const d_cell_padding = document.getElementById('cell-padding')
@@ -8,13 +12,18 @@ const d_cell_padding = document.getElementById('cell-padding')
 const d_reset = document.getElementById('reset')
 
 const game = document.getElementById('game')
+const announce = document.getElementById('announce')
 
 let game_width
 let game_height
-let mine
+let mine_amount
 let cell_width
 let cell_height
 let cell_padding
+
+let arena
+let opened
+let over
 
 function numeric_only() {
 	this.value = this.value.replace(/[^0-9]/g, '')
@@ -22,7 +31,7 @@ function numeric_only() {
 		switch (this.id) {
 		case 'game-width':
 		case 'game-height':
-		case 'mine':
+		case 'mine-amount':
 			this.value = 10
 			break
 		case 'cell-width':
@@ -35,15 +44,95 @@ function numeric_only() {
 		}
 }
 
-function open(event) {
-	classList = event.target.classList
-	if (classList.contains('cell-closed')) {
-		classList.remove('cell-closed')
-		classList.add('cell-opened')
+function add_mine(y, x) {
+	if (arena[y][x] != -1)
+		arena[y][x] += 1
+}
+
+function update_mine(y, x) {
+	const r = (x < game_width-1)
+	const l = (x > 0)
+	const u = (y < game_height-1)
+	const d = (y > 0)
+	if (r) add_mine(y, x+1)
+	if (l) add_mine(y, x-1)
+	if (u) add_mine(y+1, x)
+	if (d) add_mine(y-1, x)
+	if (r && u) add_mine(y+1, x+1)
+	if (r && d) add_mine(y-1, x+1)
+	if (l && u) add_mine(y+1, x-1)
+	if (l && d) add_mine(y-1, x-1)
+}
+
+function construct_data() {
+	arena = []
+	for (let i = 0; i < game_height; ++i) {
+		arena[i] = []
+		for (let j = 0; j < game_width; ++j)
+			arena[i][j] = 0
+	}
+
+	for (let i = 0; i < mine_amount; ++i) {
+		let x, y
+		do {
+			x = random(0, game_width-1)
+			y = random(0, game_height-1)
+		} while (arena[y][x] == -1)
+		arena[y][x] = -1
+		update_mine(y, x)
 	}
 }
 
-function construct() {
+function game_over(win) {
+	over = true
+	for (let i = 0; i < game_width * game_height; ++i) {
+		const cell = game.children[i]
+		if (cell.classList.contains('bomb'))
+			cell.classList.add('opened')
+	}
+	const message = document.createElement('h1')
+	if (win)
+		message.innerHTML = 'YOU WIN'
+	else
+		message.innerHTML = 'YOU LOSE'
+	announce.appendChild(message)
+}
+
+function open(event) {
+	if (over)
+		return
+
+	const target = event.target
+	const classList = target.classList
+	const x = target.getAttribute('x')
+	const y = target.getAttribute('y')
+
+	if (classList.contains('opened'))
+		return
+	classList.add('opened')
+
+	const symbol = document.createElement('p')
+	symbol.style['font-size'] = `${Math.min(cell_width*.75, cell_height*.75)}px`
+
+	switch (arena[y][x]) {
+	case -1:
+		game_over(false)
+		symbol.innerHTML = 'X'
+		target.appendChild(symbol)
+		break
+	case 0:
+		break
+	default:
+		symbol.innerHTML = `${arena[y][x]}`
+		target.appendChild(symbol)
+		opened += 1
+		if (opened == game_width * game_height - mine_amount)
+			game_over(true)
+		break
+	}
+}
+
+function construct_html() {
 	while (game.firstChild)
 		game.removeChild(game.firstChild)
 
@@ -54,10 +143,14 @@ function construct() {
 	for (let i = 0; i < game_height; ++i)
 		for (let j = 0; j < game_width; ++j) {
 			const cell = document.createElement('div')
+			cell.setAttribute('x', j)
+			cell.setAttribute('y', i)
 			cell.style['width'] = `${cell_width}px`
 			cell.style['height'] = `${cell_height}px`
 			cell.classList.add('cell')
-			cell.classList.add('cell-closed')
+			if (arena[i][j] == -1)
+				cell.classList.add('bomb')
+			cell.addEventListener('click', open)
 			cell.addEventListener('mousedown', open)
 			cell.addEventListener('touchstart', open)
 			game.appendChild(cell)
@@ -67,18 +160,25 @@ function construct() {
 function reset() {
 	game_width = Number(d_game_width.value)
 	game_height = Number(d_game_height.value)
-	mine = Number(d_mine.value)
+	mine_amount = Number(d_mine_amount.value)
 	cell_width = Number(d_cell_width.value)
 	cell_height = Number(d_cell_height.value)
 	cell_padding = Number(d_cell_padding.value)
-	construct()
+
+	opened = 0
+	over = false
+	while (announce.firstChild)
+		announce.removeChild(announce.firstChild)
+
+	construct_data()
+	construct_html()
 
 	console.log(`${game_width} ${game_height}`)
 }
 
 d_game_width.addEventListener('focusout', numeric_only)
 d_game_height.addEventListener('focusout', numeric_only)
-d_mine.addEventListener('focusout', numeric_only)
+d_mine_amount.addEventListener('focusout', numeric_only)
 d_cell_width.addEventListener('focusout', numeric_only)
 d_cell_height.addEventListener('focusout', numeric_only)
 d_cell_padding.addEventListener('focusout', numeric_only)
